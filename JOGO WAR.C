ce9c5507ce8b4c5cedc5baffc7c6da61ac3a64e2
EdmundoSR <desouzaribeiroedmundo@gmail.com>
2025-09-16 09:05:27 +0000

    commit

--- JOGO WAR.C
+++ JOGO WAR.C
@@ -60,7 +60,42 @@ int main (){
                 printf("digite a Tropa: ");
                 fgets(exercito[totalTerritorio].tropa, tam_string, stdin);
                 
+                exercito[totalTerritorio].nome[strcspn(exercito[totalTerritorio].nome, "\n")] = '\0';
+                exercito[totalTerritorio].cor[strcspn(exercito[totalTerritorio].cor, "\n")] = '\0'; 
+                exercito[totalTerritorio].tropa[strcspn(exercito[totalTerritorio].tropa, "\n")] = '\0'; 
+                
+                totalTerritorio++;
+
+                Printf("\nTerritorio cadastrado com sucesso!\n");
+            } else {
+                printf("exercito cheio! não é possivel cadastrar mais territorio.\n");
             }
+
+            printf("\nPresione Enter para continuar...");
+            getchar(); // pausa para o usuário ler a mensagem antes de voltar ao menu.
+            break;
+
+            case 2: // listagem de territorio
+            printf("--- Lista de territorio cadastrados ---\n\n");
+
+            if (totalTerritotio == 0) {
+                printf("Nenhum territorio cadastrado ainda.\n");
+            } elese {
+                for (int i = 0 < totalTerritorio; i++){
+                    Printf("-------------------------------\n");
+                    Printf("Territorio %d\n", 1 + 1);
+                    printf("Exército: %s\n", exercito[i].nome);
+                    printf("Cor: %s\n", exercito[i].cor);
+                    printf("Tropa: %s\n", exercito[i].tropa);
+                }
+                printf("----------------------------------\n");
+            }
+            // A pausa é crucial para que o usuário veja a lista antes
+            //do próximo loop limpar a tela.
+            printf("\nPressione enter para continuar...");
+            getchar();
+            break;
+            
         }
     }
 } 
